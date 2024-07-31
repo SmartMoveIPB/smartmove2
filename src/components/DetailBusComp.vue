73c594fe6c78926ae5659f618093e46368be2ba8
JuraganPacarCina <145534732+takumifahri@users.noreply.github.com>
2024-07-31 13:18:29 +0700

    semoga berkah , ada perubahan

--- src/components/DetailBusComp.vue
+++ src/components/DetailBusComp.vue
@@ -13,7 +13,7 @@
                     <h2>
                         TransJakarta
                     </h2>
-                    <br>
+                    <br/>
                     <p>
                         Bus TransJakarta adalah Transportasi Transit dalam kota yang di buatkan oleh Kementerian Perhubungan yang cukup terkenal di Indonesia. Mereka mengoperasikan layanan bus dalam yang terkenal dengan kenyamanan dan keamanannya. Bus TransJakarta juga dikenal memiliki armada yang cukup modern dan terawat dengan baik.
                     </p>
@@ -43,6 +43,11 @@
                         <v-icon icon="mdi-arrow-right" style="margin-top: 2px;" /> 
                     </div>
 
+                    <div class="my-10">
+                        <h3 class="text-center">TransJakarta Blok M - PGC</h3>
+                        <br>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4582.317149560476!2d106.79817100410101!3d-6.242237633610788!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f11a035ff483%3A0xd8c483ca69a8e81!2sTaman%20Literasi%20Martha%20Christina%20Tiahahu%2C%20Jakarta%20blok%20M!5e0!3m2!1sid!2sid!4v1722406207459!5m2!1sid!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
                     <!-- <div class="detail-bus ">
                         <h3 class="font-weight-regular">Detail Kendaraan :</h3>
                         <table>
